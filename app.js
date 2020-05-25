511bc2bb125a4cbd7301a167e30b16dc5490043d
wberdowski <10144015+wberdowski@users.noreply.github.com>
2020-05-25 21:05:12 +0200

    Add history api support.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', function () {
-	let category = document.querySelector(".page-header > .category");
-	let title = document.querySelector(".page-header > .title");
-	let navItems = document.querySelectorAll(".nav .item");
+	category = document.querySelector(".page-header > .category");
+	title = document.querySelector(".page-header > .title");
+	navItems = document.querySelectorAll(".nav .item");
 	
 	for(let i = 0; i < navItems.length; i++){
 		let item = navItems[i];
@@ -18,59 +18,64 @@ window.addEventListener('load', function () {
 			setSelected(item);
 		});
 	}
+});
 
-	function setSelected(item){
-		let target = item.getAttribute("data");
-		location.hash = target.substr(6);
-		loadSubpage(target);
-		uncheckAllItems(navItems);
-		item.setAttribute("selected", "");
+function setSelected(item){
+	let target = item.getAttribute("data");
+	location.hash = target.substr(6);
+	loadSubpage(target);
+	uncheckAllItems(navItems);
+	item.setAttribute("selected", "");
+}
+
+function uncheckAllItems(items){
+	for(let i = 0; i < items.length; i++){
+		items[i].removeAttribute("selected");
 	}
+}
 
-	function uncheckAllItems(items){
-		for(let i = 0; i < items.length; i++){
-			items[i].removeAttribute("selected");
+function loadSubpage(url){
+	loadMeta(url, function(meta){
+		title.textContent = meta.title;
+		// Load body
+		sendGetReq(url + "/body.html",function(req){
+			let text = req.responseText;
+			document.querySelector(".page-content").innerHTML = text;
+			
+			// Load script
+			loadScripts(url, meta);
+		});		
+	});
+}
+
+function sendGetReq(url, callback){
+	let req = new XMLHttpRequest();
+	req.open("GET", url, true); 
+	req.onreadystatechange = function(){
+		if(req.readyState == XMLHttpRequest.DONE && req.status == 200) {
+			callback(req);
 		}
 	}
+	req.send(null);
+}
 
-	function loadSubpage(url){
-		loadMeta(url, function(meta){
-			title.textContent = meta.title;
-			// Load body
-			sendGetReq(url + "/body.html",function(req){
-				let text = req.responseText;
-				document.querySelector(".page-content").innerHTML = text;
-				
-				// Load script
-				loadScripts(url, meta);
-			});		
+function loadMeta(url, callback){
+	sendGetReq(url + "/page-meta.json", function(req){
+		callback(JSON.parse(req.responseText));
+	});
+}
+
+function loadScripts(url, meta){
+	let scripts = meta.path.scripts;
+	for(let i = 0; i < scripts.length; i++){
+		sendGetReq(url + "/" + scripts[i], function(req2){
+			let text = req2.responseText;
+			eval(text);
 		});
 	}
+}
 
-	function sendGetReq(url, callback){
-		let req = new XMLHttpRequest();
-		req.open("GET", url, true); 
-		req.onreadystatechange = function(){
-			if(req.readyState == XMLHttpRequest.DONE && req.status == 200) {
-				callback(req);
-			}
-		}
-		req.send(null);
-	}
-
-	function loadMeta(url, callback){
-		sendGetReq(url + "/page-meta.json", function(req){
-			callback(JSON.parse(req.responseText));
-		});
-	}
-
-	function loadScripts(url, meta){
-		let scripts = meta.path.scripts;
-		for(let i = 0; i < scripts.length; i++){
-			sendGetReq(url + "/" + scripts[i], function(req2){
-				let text = req2.responseText;
-				eval(text);
-			});
-		}
-	}
+window.addEventListener('popstate', function (e) {
+	let item = document.querySelector(".nav .item[data=\"pages/" + location.hash.substr(1) + "\"]");
+	setSelected(item);
 });
